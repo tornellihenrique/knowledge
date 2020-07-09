d8fdea632fcb6b2f4520fa4afdb03fc511f9623c
Henrique Tornelli Duarte <henrique.tornelli@ilinksolutions.com.br>
2020-07-09 20:16:50 -0300

    configuracao de base de dados iniciando classes user e category

---
+++ backend/api/category.js
@@ -0,0 +1,62 @@
+module.exports = app => {
+    const { existsOrError, notExistsOrError, equalsOrError } = app.api.validation
+
+    const save = (req, res) => {
+        const category = { ...req.body }
+
+        if (req.params.id) category.id = req.params.id
+
+        try {
+            existsOrError(category.name, 'Nome não informado')
+        } catch (msg) {
+            return res.status(400).send(msg)
+        }
+
+        if (category.id) {
+            app.db('categories')
+                .update(category)
+                .where({ id: category.id })
+                .then(_ => res.status(204).send())
+                .catch(err => res.status(500).send(err))
+        } else {
+            app.db('categories')
+                .insert(category)
+                .then(_ => res.status(204).send())
+                .catch(err => res.status(500).send(err))
+        }
+    }
+
+    const remove = (req, res) => {
+        try {
+            existsOrError(req.params.id, 'Código da categoria não informado')
+
+            const subcategory = await app.db('categories')
+                .where({ parentId: req.params.id })
+
+            notExistsOrError(subcategory, 'Categoria possui subcategorias')
+
+            const articles = await app.db('articles')
+                .where({ categoryId: req.params.id })
+
+            notExistsOrError(articles, 'Categoria possui artigos')
+
+            const rowsDeleted = await app.db('categories')
+                .where({ id: req.params.id }).del()
+
+            existsOrError(rowsDeleted, 'Categoria não foi encontrada')
+
+            res.status(204).send()
+        } catch (msg) {
+            res.status(500).send(msg)
+        }
+    }
+
+    const withPath = categories => {
+        const getParent = (categories, parentId) => {
+            let parent = categories.filter(p => p.id === parentId)
+            return parent.length ? parent[0] : null;
+        }
+
+        
+    }
+}
