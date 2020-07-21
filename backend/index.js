4f9c71361d4bb80a35c6162ec2f65d45d89146f4
Henrique Tornelli Duarte <henrique.tornelli@ilinksolutions.com.br>
2020-07-21 14:41:54 -0300

    sistema de tokens e administradores

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,13 @@ const db = require('./config/db')
 
 app.db = db
 
-consign().then('./config/middlewares.js').then('./api/validation.js').then('./api').then('./config/routes.js').into(app)
+consign()
+	.include('./config/passport.js')
+	.then('./config/middlewares.js')
+	.then('./api/validation.js')
+	.then('./api')
+	.then('./config/routes.js')
+	.into(app)
 
 app.listen(3000, () => {
 	console.info('Backend executando...')
